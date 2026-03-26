5d4c461a91ec89f88057a2c3975bb996e11f9979
w2201 <w22012025@gmail.com>
2026-03-26 12:55:39 +0100

    init: structure projet Node.js + Express + première route

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//Middleware pour lire le JSON
+app.use(express.json());
+
+//Route de test
+app.get("/", (req, res) => {
+  res.json({ message: "Bienvenue sur StepByDev!🚀" });
+});
+
+//Démarrage du serveur
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
+});
